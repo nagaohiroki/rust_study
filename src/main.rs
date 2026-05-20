85fbda420451ae9b87ab7d452ec1692b23149e87
nagaohiroki <testplayers0226@gmail.com>
2026-05-20 09:36:26 +0900

    add wgpu

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use wgpu;
+
 fn main() {
-    println!("Hello, world!");
+    pollster::block_on(run());
+}
+async fn run() {
+    let instance = wgpu::Instance::default();
+    let adapter = instance
+        .request_adapter(&wgpu::RequestAdapterOptions {
+            power_preference: wgpu::PowerPreference::HighPerformance,
+            ..Default::default()
+        })
+        .await
+        .expect("not found gpu");
+    let info = adapter.get_info();
+    println!("usage GPU: {}", info.name);
+    println!("backend: {:?}", info.backend);
 }
