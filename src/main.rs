d31407a0c51ddbb8aa265b3800f909b24ef5b5b4
nagaohiroki <testplayers0226@gmail.com>
2026-05-20 19:58:50 +0900

    add open_window

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,32 @@
-use wgpu;
-
+﻿use wgpu;
+use winit::{
+    event::{Event, WindowEvent},
+    event_loop::EventLoop,
+    window::WindowBuilder,
+};
 fn main() {
     pollster::block_on(run());
+    open_window();
+}
+fn open_window() {
+    let event_loop = EventLoop::new().unwrap();
+    let window = WindowBuilder::new()
+        .with_title("Rust wgpu Window")
+        .with_inner_size(winit::dpi::LogicalSize::new(800.0, 600.0))
+        .build(&event_loop)
+        .unwrap();
+    event_loop
+        .run(move |event, control_flow| match event {
+            Event::WindowEvent {
+                ref event,
+                window_id,
+            } if window_id == window.id() => match event {
+                WindowEvent::CloseRequested => control_flow.exit(),
+                _ => {}
+            },
+            _ => {}
+        })
+        .unwrap();
 }
 async fn run() {
     let instance = wgpu::Instance::default();
