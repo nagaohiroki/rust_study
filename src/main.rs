acf2bba3a13934f71e1dbe5a7792ab93c555ef0f
nagaohiroki <testplayers0226@gmail.com>
2026-05-27 20:06:16 +0900

    add vertex buffer

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,48 @@ struct State {
     queue: wgpu::Queue,
     config: wgpu::SurfaceConfiguration,
     render_pipeline: wgpu::RenderPipeline,
+    vertex_buffer: wgpu::Buffer,
 }
+#[repr(C)]
+#[derive(Copy, Clone, Debug, bytemuck::Pod, bytemuck::Zeroable)]
+struct Vertex {
+    position: [f32; 3],
+    color: [f32; 3],
+}
+impl Vertex {
+    fn desc() -> wgpu::VertexBufferLayout<'static> {
+        wgpu::VertexBufferLayout {
+            array_stride: std::mem::size_of::<Vertex>() as wgpu::BufferAddress,
+            step_mode: wgpu::VertexStepMode::Vertex,
+            attributes: &[
+                wgpu::VertexAttribute {
+                    offset: 0,
+                    shader_location: 0,
+                    format: wgpu::VertexFormat::Float32x3,
+                },
+                wgpu::VertexAttribute {
+                    offset: std::mem::size_of::<[f32; 3]>() as wgpu::BufferAddress,
+                    shader_location: 1,
+                    format: wgpu::VertexFormat::Float32x3,
+                },
+            ],
+        }
+    }
+}
+const VERTICES: &[Vertex] = &[
+    Vertex {
+        position: [0.0, 0.5, 0.0],
+        color: [1.0, 1.0, 0.0],
+    },
+    Vertex {
+        position: [-0.5, -0.5, 0.0],
+        color: [0.0, 1.0, 0.0],
+    },
+    Vertex {
+        position: [0.5, -0.5, 0.0],
+        color: [0.0, 0.0, 1.0],
+    },
+];
 impl State {
     fn resize(&mut self, new_size: &winit::dpi::PhysicalSize<u32>) {
         self.config.width = new_size.width;
@@ -54,7 +95,7 @@ async fn init_wgpu(window: Arc<winit::window::Window>) -> State {
         vertex: wgpu::VertexState {
             module: &shader,
             entry_point: "vs_main",
-            buffers: &[],
+            buffers: &[Vertex::desc()],
         },
         fragment: Some(wgpu::FragmentState {
             module: &shader,
@@ -70,12 +111,19 @@ async fn init_wgpu(window: Arc<winit::window::Window>) -> State {
         multisample: wgpu::MultisampleState::default(),
         multiview: None,
     });
+    use wgpu::util::DeviceExt;
+    let vertex_buffer = device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
+        label: Some("Vertex Buffer"),
+        contents: bytemuck::cast_slice(VERTICES),
+        usage: wgpu::BufferUsages::VERTEX,
+    });
     State {
         surface,
         device,
         queue,
         config,
         render_pipeline,
+        vertex_buffer,
     }
 }
 fn render(state: &State) {
@@ -107,6 +155,7 @@ fn render(state: &State) {
             occlusion_query_set: None,
         });
         render_pass.set_pipeline(&state.render_pipeline);
+        render_pass.set_vertex_buffer(0, state.vertex_buffer.slice(..));
         render_pass.draw(0..3, 0..1);
     }
     state.queue.submit(std::iter::once(encoder.finish()));
